bf2fc0253cc34ee91be97900dcf4764cd1f2fe10
SNIAPA <hubertwasilewski69@gmail.com>
2021-05-16 18:03:38 +0200

    File saving

--- app/src/main/java/com/n_kodemandsniapa/energycollector/FirstFragment.kt
+++ app/src/main/java/com/n_kodemandsniapa/energycollector/FirstFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_first.*
@@ -19,13 +18,18 @@ class FirstFragment : Fragment() {
 
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
 
     }
+
+    fun update(s: String?) {
+        locationView.setText(s)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
